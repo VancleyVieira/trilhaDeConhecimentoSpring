289ac0b6dbaec9f93dc59e3afcf2bae26c63abfa
Vancley <vancley.vieira@fatec.sp.gov.br>
2023-08-13 11:08:17 -0300

    criacao de um novo projeto utilizando Spring Initializr

---
+++ trilhaDeConhecimentoSpring/src/main/java/com/trilhadeconhecimentospring/TrilhaDeConhecimentoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.trilhadeconhecimentospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrilhaDeConhecimentoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrilhaDeConhecimentoSpringApplication.class, args);
+	}
+
+}
---
+++ trilhaDeConhecimentoSpring/src/test/java/com/trilhadeconhecimentospring/TrilhaDeConhecimentoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trilhadeconhecimentospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrilhaDeConhecimentoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
